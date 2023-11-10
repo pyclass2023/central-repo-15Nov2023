67ebaea6e21eaeabb0dcfc74efb8eb837c6b5330
kdk <kapilkale@gmail.com>
2023-11-10 10:11:09 +0530

    F

--- p2.py
+++ p2.py
@@ -1,3 +1,7 @@
 i=10
 y=20
 print(i+y)
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
